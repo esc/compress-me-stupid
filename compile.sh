3ef33d4816504fa8340418768e183f2179470937
Valentin Haenel <valentin.haenel@gmx.de>
2014-07-22 23:37:26 +0200

    remove old compile script

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-pandoc -t beamer slides.rst -o slides.tex --filter ./pygments.hs -H borland-defs.tex --template beamer_template.tex
-sed s/begin{frame}/begin{frame}[fragile]/ slides.tex > slides-fragile.tex
-pdflatex slides-fragile.tex
-mv slides-fragile.pdf slides.pdf
